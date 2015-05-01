cb2ebfa426f548e99cd35e2367a9ef61a785c1e0
Vale <Vale@valentinik.Home>
2015-05-01 19:56:48 +0200

    Initial version of example's project

---
+++ BasicsExamplesJSE/src/val/examples/basic/LongestWordRec.java
@@ -0,0 +1,44 @@
+package val.examples.basic;
+
+
+/**
+ * This class is a recursive version that lets you know which of parameters received is the longest
+ * 
+ * @author Val
+ *
+ */
+public class LongestWordRec {
+	
+	private static String longerParam (int position, String[] args, String longerString )
+	
+	{
+		
+		if (position<args.length)
+		{
+			if (args[position].length()>longerString.length())
+			{
+				longerString = longerParam(position+1, args, args[position]);
+			} else longerString = longerParam(position+1, args, longerString);
+		}
+		
+		return longerString;	
+		
+	}
+	
+	public static void main(String[] args) {
+		String longest = null;
+		
+		
+		try {
+			
+			longest = longerParam(1, args, args[0]);
+			System.out.println("The longest arugment recieved is " + longest);
+			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			e.printStackTrace();
+			System.err.println("At least, you should pass one argument invoking this program");
+		}
+		
+	}
+
+}
