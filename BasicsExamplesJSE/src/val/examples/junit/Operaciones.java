8e0ea53d7acff1b9768517b115ba7565e6f28924
Val <valexx55@gmail.com>
2015-05-27 18:48:04 +0200

    Operaciones JUNIT

---
+++ BasicsExamplesJSE/src/val/examples/junit/Operaciones.java
@@ -0,0 +1,24 @@
+package val.examples.junit;
+
+public class Operaciones {
+
+	public int suma (int a , int b)
+	{
+		return a+b;
+	}
+	
+	public int multiplicacion (int a , int b)
+	{
+		return a*b;
+	}
+	
+	public int division (int a , int b)
+	{
+		return a/b;
+	}
+	
+	public int resta (int a , int b)
+	{
+		return a-b;
+	}
+}
