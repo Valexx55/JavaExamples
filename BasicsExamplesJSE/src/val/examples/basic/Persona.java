700468f12be8157ac9645ae50e0f128f4f916159
Val <valexx55@gmail.com>
2015-05-28 14:10:17 +0200

    Update Persona

--- BasicsExamplesJSE/src/val/examples/basic/Persona.java
+++ BasicsExamplesJSE/src/val/examples/basic/Persona.java
@@ -46,4 +46,41 @@ public class Persona implements Serializable, Comparable{
 		
 		return this.edad-p_aux.edad;
 	}	
+	
+	/**
+	 * Comprueba si el objeto es de tipo persona
+	 * @param obj
+	 * @return un objeto de Persona si obj era de tipo Persona o null, si obj no era de tipo Persona
+	 */
+	private Persona esTipoPersona (Object obj)
+	{
+		Persona p = null;
+		
+		if (obj instanceof Persona) {
+			 p = (Persona) obj;
+		}
+		
+		return p;
+		
+	}
+	
+	private static boolean contienenLoMismo (Persona p1, Persona p2)
+	{
+		boolean contienen_lomismo = false;
+		
+		
+		contienen_lomismo = (p1.nombre.equals(p2.nombre)&&(p1.edad==p2.edad));
+		
+		return contienen_lomismo;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		boolean dev= false;
+		Persona p = null;
+		
+		dev = (null!=obj)&&((obj == this)||((null!=(p=esTipoPersona(obj)))&&(contienenLoMismo(this, p))));
+	
+		return (dev);
+	}
 }
