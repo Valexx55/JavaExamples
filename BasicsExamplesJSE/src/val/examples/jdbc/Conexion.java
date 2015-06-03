b666d3bef5215a2399539c912bac9ab61bb78812
Val <valexx55@gmail.com>
2015-06-03 23:36:48 +0200

    Ejemplo Clase con sección estática -- Static section example

--- BasicsExamplesJSE/src/val/examples/jdbc/Conexion.java
+++ BasicsExamplesJSE/src/val/examples/jdbc/Conexion.java
@@ -4,60 +4,63 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-
 import org.apache.log4j.Logger;
 
+
+/**
+ * Si queréis que adaptar esta clase a vuestra práctica, sólo deberíais modificar
+ * el fichero de propiedades ajustándolo a vuestras necesidades (el de log y el de db
+ * 
+ * Ya no es Singleton, pero incluye la sección de código static, para que tengáis un ejemplo
+ * de otra cosita nueva ;)
+ * 
+ * @date 03-06-2015
+ * @author Vale
+ *
+ */
 public class Conexion {
 
 	
-	private static Conexion conexion = new Conexion();
-	private Connection connection;
-	private final String RUTA_PROPERTIES = "db.properties";
-	private Logger log = Logger.getLogger("mylog");
+	private static final String RUTA_PROPERTIES = "db.properties";
+	private static Logger log = Logger.getLogger("mylog");
+	private static String cadena_conexion; 
+	private static String usuario;
+	private static String contrasenia;
 	
-	
-	
-	private Conexion () {
+	static{
+		//el bloque de código encerrado en esta llaves static 
+		//sólo se ejecutará una vez; la primera que es invocada esta clase Conexion!!!
 		
 		Properties properties = new Properties();
 		try {
 			properties.load(new FileReader(RUTA_PROPERTIES));
 			String s_driver = properties.getProperty("driver");
-			String cadena_conexion = properties.getProperty("cadena_conexion");
-			String usuario = properties.getProperty("user");
-			String contrasenia = properties.getProperty("password");
+			cadena_conexion = properties.getProperty("cadena_conexion");
+			usuario = properties.getProperty("user");
+			contrasenia = properties.getProperty("password");
 			Class.forName(s_driver);
-			DriverManager.registerDriver((Driver) Class.forName(s_driver).newInstance());
-			this.connection = DriverManager.getConnection(cadena_conexion, usuario, contrasenia);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			log.error("Fichero de properties no encontrado");
 		} catch (IOException e) {
 			e.printStackTrace();
 			log.error("Fichero de properties no encontrado");
-		} catch (SQLException e) {
-			e.printStackTrace();
-			log.error("Error al intentar conectar a la base de datos");
 		} catch (ClassNotFoundException e) {
 			log.error("Error al intentar conectar a la base de datos: driver no encontrado");
-		} catch (InstantiationException e) {
-			log.error("Error al instanciar el driver");
-		} catch (IllegalAccessException e) {
-			log.error("Error al instanciar el driver");
 		}
 		
-				
 	}
 	
-	public static Connection obtenerConexion ()
+	
+	public static Connection obtenerConexion () throws SQLException
 	{
-		return conexion.connection;
+		//bug corregido --> ahora devuelvo una nueva conexión cada vez que me lo piden
+		return DriverManager.getConnection(cadena_conexion, usuario, contrasenia);
 	}
 	
 	public static void liberarRecursos (Connection conn, Statement stmt, ResultSet rset)
@@ -65,5 +68,13 @@ public class Conexion {
 		if (rset != null) 	{ try { rset.close(); } catch (Exception e2) { e2.printStackTrace(); }}
 		if (stmt != null)	{ try {	stmt.close(); } catch (Exception e2) { e2.printStackTrace(); }}
 		if (conn != null) 	{ try { conn.close(); } catch (Exception e3) { e3.printStackTrace(); }}
+	}
+	
+	//nuevo método, sobrecarga del anterior, cuando necesitáis liberar recursos, después
+	//de ejecutar una instrucción que NO es un SELECT ;)
+	public static void liberarRecursos (Connection conn, Statement stmt)
+	{
+		if (rset != null) 	{ try { rset.close(); } catch (Exception e2) { e2.printStackTrace(); }}
+		if (stmt != null)	{ try {	stmt.close(); } catch (Exception e2) { e2.printStackTrace(); }}
 	}
 }
