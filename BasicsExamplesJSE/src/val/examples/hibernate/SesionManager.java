7cd415e6e3d72a52e0fe221a7fa80363b0ac8dda
Val <valexx55@gmail.com>
2015-06-11 19:17:35 +0200

    Session Factory - Session Manager - DAO

---
+++ BasicsExamplesJSE/src/val/examples/hibernate/SesionManager.java
@@ -0,0 +1,41 @@
+package val.examples.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class SesionManager {
+	
+	static {
+		
+		Configuration configuration = new Configuration().configure();
+    	StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+    	sesion_factory = configuration.buildSessionFactory(builder.build());
+		
+	}
+	
+	private static SessionFactory sesion_factory;
+	
+	
+	private SesionManager (){}
+	
+	public static SessionFactory getSessionFactory ()
+	{
+		return sesion_factory;
+	}
+	
+	public static Session obtenerSesionNueva ()
+	{
+		return sesion_factory.openSession();
+	}
+	
+	
+	public static void cerrarSession (Session sesion)
+	{
+		sesion.close();
+	}
+	
+	
+
+}
